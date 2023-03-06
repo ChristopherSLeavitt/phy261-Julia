4ef4e31d60fbe82fb933f48f1cfc56df998665f6
Paul Nakroshis <pauln@maine.edu>
2023-03-06 16:24:37 -0500

    First commit for class_11.jl

---
+++ LectureNotes/Class_11/Class_11.jl
@@ -0,0 +1,14 @@
+### A Pluto.jl notebook ###
+# v0.19.22
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 698db7c8-bc64-11ed-0488-07bca5f31a66
+md"# Elaborating on the simple harmonic oscillator
+Today, we will make the simple harmonic oscillator you just finished in 
+Assignment_04 a little more complicated. 
+"
+
+# ╔═╡ Cell order:
+# ╠═698db7c8-bc64-11ed-0488-07bca5f31a66
